2171a909ad9fd2ae12bd3d777c1a4bc0c9d224a1
Adnan Amin <adnancse23@hotmail.com>
2022-03-22 17:27:37 +0600

    bank and bank branch update

--- WebApp/src/app/features/administrative/bank/bank.service.ts
+++ WebApp/src/app/features/administrative/bank/bank.service.ts
@@ -2,72 +2,58 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IPConfiguration } from '../../../core/services/IP-configuration.service';
-import Bank from './bank.model';
-
 
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 
 export class BankService {
 
-  constructor(private http: HttpClient,private ipconfig: IPConfiguration) { }
+    constructor(private http: HttpClient, private ipconfig: IPConfiguration) { }
 
 
-  getAllBank(): Observable<Bank[]> {
-    return this.http.get<Bank[]>(this.ipconfig.base_IP + 'Bank/GetAllBanks', httpOptions);
-  }
+    getAllBank(): Observable<any[]> {
+        return this.http.get<any[]>(this.ipconfig.base_IP + 'Bank/GetAllBanks', httpOptions);
+    }
 
-  CreateBank(bank: any): Observable<Bank> {
-    console.log(bank)
-    return this.http.post<Bank>(this.ipconfig.base_IP + 'Bank/Create', bank,httpOptions);
-  }
+    CreateBank(bank: any): Observable<any> {
+        return this.http.post<any>(this.ipconfig.base_IP + 'Bank/Create', bank, httpOptions);
+    }
 
-  UpdateBank(bank: any): Observable<Bank> {
- 
-    return this.http.post<Bank>(this.ipconfig.base_IP + 'Bank/Update',bank,httpOptions);
-  }
-  
-
-  DeleteBank(bank_id: Number): Observable<any> {
-    return this.http.post<any>(this.ipconfig.base_IP + 'Bank/Delete', { bank_id }, httpOptions);
-
-  }
-
-  //DeleteUser(user_info_id: Number): Observable<any> {
-
-  //  return this.http.post<any>(this.ipconfig.base_IP + 'User/Delete', { user_info_id }, httpOptions);
-
-  //}
+    UpdateBank(bank: any): Observable<any> {
+        return this.http.post<any>(this.ipconfig.base_IP + 'Bank/Update', bank, httpOptions);
+    }
 
 
+    DeleteBank(bank_id: Number): Observable<any> {
+        return this.http.post<any>(this.ipconfig.base_IP + 'Bank/Delete', { bank_id }, httpOptions);
+    }
 
-  GetBankById(bank_id: Number): Observable<any> {
+    GetBankById(bank_id: Number): Observable<any> {
+        return this.http.get(this.ipconfig.base_IP + 'Bank/GetBankById?bank_id=' + bank_id, httpOptions);
+    }
 
-    return this.http.get(this.ipconfig.base_IP + 'Bank/GetBankById?bank_id=' + bank_id, httpOptions);
-  }
+    getAllCountryCboList(): Observable<any[]> {
+        return this.http.get<any[]>(this.ipconfig.base_IP + 'Country/CountryCboList');
+    }
 
-  getAllCountryCboList(): Observable<Bank[]> {
-    return this.http.get<Bank[]>(this.ipconfig.base_IP + 'Country/CountryCboList');
-  }
+    getAllDivisionCboList(): Observable<any[]> {
+        return this.http.get<any[]>(this.ipconfig.base_IP + 'Division/DivisionCboList');
+    }
 
-  getAllDivisionCboList(): Observable<Bank[]> {
-    return this.http.get<Bank[]>(this.ipconfig.base_IP + 'Division/DivisionCboList');
-  }
+    getAllDistrictCboList(): Observable<any[]> {
+        return this.http.get<any[]>(this.ipconfig.base_IP + 'District/DistrictCboList');
+    }
 
-  getAllDistrictCboList(): Observable<Bank[]> {
-    return this.http.get<Bank[]>(this.ipconfig.base_IP + 'District/DistrictCboList');
-  }
-
-  getAllDivisionCboListByCountryId(country_id): Observable<Bank[]> {
-    return this.http.get<Bank[]>(this.ipconfig.base_IP + 'Division/DivisionCboListByCountryId?country_id=' + country_id, httpOptions);
-  }
-  getAllDistrictCboListByDivisionId(division_id): Observable<Bank[]> {
-    return this.http.get<Bank[]>(this.ipconfig.base_IP + 'District/DistrictCboListByDivisionId?division_id=' + division_id, httpOptions);
-  }
+    getAllDivisionCboListByCountryId(country_id): Observable<any[]> {
+        return this.http.get<any[]>(this.ipconfig.base_IP + 'Division/DivisionCboListByCountryId?country_id=' + country_id, httpOptions);
+    }
+    getAllDistrictCboListByDivisionId(division_id): Observable<any[]> {
+        return this.http.get<any[]>(this.ipconfig.base_IP + 'District/DistrictCboListByDivisionId?division_id=' + division_id, httpOptions);
+    }
 }
