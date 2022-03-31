68a5dc73a9e4efed894bd483ec32b99bc4a7cf3d
Al-Amin Mollick <alamin.renessa@gmail.com>
2022-03-31 17:56:11 +0600

    Attendance policy file added

---
+++ Auth/Model/Attendance/Model/AttendancePolicy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Model.Attendance.Model
+{
+    public class AttendancePolicy
+	{
+    public int attendance_policy_id { get; set; }
+	public string code { get; set; }
+	public string policy_name { get; set; }
+	public string remarks { get; set; }
+	public int attendance_calendar_id { get; set; }
+	public int late_early_policy_id { get; set; }
+	public int absenteeism_policy_id { get; set; }
+	public int roster_policy_id { get; set; }
+	public bool is_random_dayoff { get; set; }
+	public int no_of_random_dayoff { get; set; }
+	public bool is_allow_benefit_policy { get; set; }
+	public List<AttendancePolicyDayoff> attendance_Policy_Dayoffs { get; set; }
+    public List<AttendancePolicyBenefit> attendance_Policy_Benefits { get; set; }
+	public List<AttendancePolicyLeave> attendance_Policy_Leaves { get; set; }
+	public List<AttendancePolicyShift> attendance_Policy_Shifts { get; set; }
+	}
+
+}
---
+++ Auth/Model/Attendance/Model/AttendancePolicyDayoff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Model.Attendance.Model
+{
+    public class AttendancePolicyDayoff
+    {
+        public int attendance_policy_dayoff_id { get; set; }
+        public int attendance_policy_id { get; set; }
+        public string week_day { get; set; }
+        public byte dayoff_type_id { get; set; }
+        public byte dayoff_alternative_id { get; set; }
+    }
+}
