765ba67044830b055b32220865eab07fc03505c2
Adnan Amin <adnancse23@hotmail.com>
2022-04-11 02:24:14 -0700

    supplier assessment

---
+++ WebApp/src/app/features/Procurement/supplierlist/criteria.ts
@@ -0,0 +1,8 @@
+export interface Criteria {
+    supplier_master_information_id?: number;
+/*    code?: number;*/
+    supplier_master_information_name?: string;
+    manual_weight?: number;
+    actual_weight?: number;
+    comments?: string;
+}
