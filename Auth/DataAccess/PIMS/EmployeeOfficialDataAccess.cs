9050c3180c08314f147ebb9453c8c65f6305461e
Md. Masud Iqbal <iqbaldiit@gmail.com>
2022-04-13 16:10:23 +0600

    Employee Official data access and model

---
+++ Auth/DataAccess/PIMS/EmployeeOfficialDataAccess.cs
@@ -0,0 +1,65 @@
+﻿using Auth.Model.PIMS.Model;
+using Auth.Model.PIMS.ViewModel;
+using Auth.Utility;
+using Auth.Utility.PIMS.Enum;
+using Dapper;
+using DataAccess;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.DataAccess.PIMS
+{
+    public class EmployeeOfficialDataAccess
+    {
+        private readonly IDbConnection _dbConnection;
+        IHttpContextAccessor _httpContextAccessor = new HttpContextAccessor();
+
+        public EmployeeOfficialDataAccess(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        //Parameter binding
+        public DynamicParameters EmployeeOfficialParameterBinding(EmployeeOfficial oEmployeeOfficial, int nOperationType)
+        {
+            var currentUserInfoId = _httpContextAccessor.HttpContext.Items["User_Info_Id"];
+            var company_id = _httpContextAccessor.HttpContext.Items["company_id"];
+            var company_corporate_id = _httpContextAccessor.HttpContext.Items["company_corporate_id"];
+            var company_group_id = _httpContextAccessor.HttpContext.Items["company_group_id"];
+
+            DynamicParameters parameters = new DynamicParameters();
+
+            if (nOperationType == (int)GlobalEnumList.DBOperation.Create || nOperationType == (int)GlobalEnumList.DBOperation.Update)
+            {
+                parameters.Add("@param_employee_id", oEmployeeOfficial.employee_id, DbType.Int64);
+                parameters.Add("@param_organogram_detail_id", oEmployeeOfficial.organogram_detail_id, DbType.Int32);
+                parameters.Add("@param_company_id", oEmployeeOfficial.company_id, DbType.Int32);
+                parameters.Add("@param_location_id", oEmployeeOfficial.location_id, DbType.Int32);
+                parameters.Add("@param_department_id", oEmployeeOfficial.department_id, DbType.Int32);
+                parameters.Add("@param_position_id", oEmployeeOfficial.position_id, DbType.Int32);
+                parameters.Add("@param_designation_id", oEmployeeOfficial.designation_id, DbType.Int32);
+                parameters.Add("@param_job_domicile_id", oEmployeeOfficial.job_domicile_id, DbType.Int32);
+                parameters.Add("@param_service_type_id", oEmployeeOfficial.service_type_id, DbType.Int32);
+                parameters.Add("@param_confirmation_status_id", oEmployeeOfficial.confirmation_status_id, DbType.Int32);
+                parameters.Add("@param_working_action_id", oEmployeeOfficial.working_action_id, DbType.Int32);
+                parameters.Add("@param_job_location_id", oEmployeeOfficial.job_location_id, DbType.Int32);
+                parameters.Add("@param_date_of_join", oEmployeeOfficial.date_of_join, DbType.DateTime);
+                parameters.Add("@param_date_of_confirmation", oEmployeeOfficial.date_of_confirmation, DbType.DateTime);
+                parameters.Add("@param_created_user_id", oEmployeeOfficial.created_user_id, DbType.Int32);
+                parameters.Add("@param_DBOperation", nOperationType == (int)GlobalEnumList.DBOperation.Create ? GlobalEnumList.DBOperation.Create : GlobalEnumList.DBOperation.Update);
+            }
+            else if (nOperationType == (int)GlobalEnumList.DBOperation.Delete)
+            {
+                parameters.Add("@param_employee_id", oEmployeeOfficial.employee_id, DbType.Int64);
+                parameters.Add("@param_DBOperation", GlobalEnumList.DBOperation.Delete);
+            }
+
+            return parameters;
+        }
+
+    }
+}
---
+++ Auth/Model/PIMS/Model/EmployeeOfficial.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Auth.Model.PIMS.Model
+{
+    public class EmployeeOfficial
+    {
+        public EmployeeOfficial()
+        {
+            employee_id = 0;
+            organogram_detail_id = 0;
+            company_id = 0;
+            location_id = 0;
+            department_id = 0;
+            position_id = 0;
+            designation_id = 0;
+            job_domicile_id = 0;
+            service_type_id = 0;
+            confirmation_status_id = 0;
+            working_action_id = 0;
+            job_location_id = 0;
+            date_of_join = DateTime.Now;
+            date_of_confirmation = DateTime.Now;
+            created_user_id = 0;
+
+        }
+
+        public int? employee_id { get; set; }
+        public int? organogram_detail_id { get; set; }
+        public int? company_id { get; set; }
+        public int? location_id { get; set; }
+        public int? department_id { get; set; }
+        public int? position_id { get; set; }
+        public int? designation_id { get; set; }
+        public int? job_domicile_id { get; set; }
+        public int? service_type_id { get; set; }
+        public int? confirmation_status_id { get; set; }
+        public int? working_action_id { get; set; }
+        public int? job_location_id { get; set; }
+        public DateTime? date_of_join { get; set; }
+        public DateTime? date_of_confirmation { get; set; }
+        public int? created_user_id { get; set; }
+    }
+}
